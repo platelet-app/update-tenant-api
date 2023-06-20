a55a3ac81620450ed64649ee95d037490148c194
Theo Cranmore <theo.cranmore@gmail.com>
2023-06-20 17:55:07 +0100

    workflow

--- dist/index.js
+++ dist/index.js
@@ -2688,19 +2688,10 @@ exports["default"] = _default;
 
 /***/ }),
 
-/***/ 258:
+/***/ 507:
 /***/ ((module) => {
 
-let wait = function (milliseconds) {
-  return new Promise((resolve) => {
-    if (typeof milliseconds !== 'number') {
-      throw new Error('milliseconds not a number');
-    }
-    setTimeout(() => resolve("done!"), milliseconds)
-  });
-};
-
-module.exports = wait;
+module.exports = eval("require")("./aws-exports");
 
 
 /***/ }),
@@ -2835,20 +2826,13 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(186);
-const wait = __nccwpck_require__(258);
-
+const awsExports = __nccwpck_require__(507);
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const awsExportsFile = JSON.stringify(awsExports);
+    console.log(`AWS Exports: ${awsExportsFile}`);
   } catch (error) {
     core.setFailed(error.message);
   }
